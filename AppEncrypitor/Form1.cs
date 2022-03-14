098163db72a6cc8e8d8aa8efaf81f40a87e89c4b
Edson de Araujo <earaujo@prodesp.fazenda.sp.gov.br>
2022-03-14 15:03:30 -0300

    Primeiro Commit

---
+++ AppEncrypitor/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppEncrypitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var nome2 = "";
+            if (comboBox1.Text == "Encrypitar")
+            {
+                nome2 = txtNome.Text.ToCrypt();
+                //txtResultado.Text = nome2;
+                textRes.Text = nome2;
+
+            }
+            else
+            {
+                nome2 = txtNome.Text.FromCript();
+                //txtResultado.Text = nome2;
+                textRes.Text = nome2;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = "Encrypitar";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            textRes.Text = "";
+            txtNome.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textRes.Text);
+        }
+    }
+}
---
+++ AppEncrypitor/Seguranca.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AppEncrypitor
+{
+    public static class Seguranca
+    {
+        private static byte[] key = Convert.FromBase64String("wLJcecy1V8O+2xfZN6Sb3+9PRSHEpuZcTgihIN2I904=");
+        private static byte[] iv = Convert.FromBase64String("JTfM6sPKRElOe5oqEJrivA==");
+
+        private static string RemoveAcento(this string palavra)
+        {
+            palavra = palavra.Normalize(NormalizationForm.FormD);
+            var chars = palavra.Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark).ToArray();
+            return new string(chars).Normalize(NormalizationForm.FormC);
+        }
+
+        public static string ToCrypt(this string aux)
+        {
+            aux = aux.RemoveAcento();
+
+            return String.Join(" ", aux.Split(' ').Select(s => s.Encrypt(key, iv)));
+        }
+
+        public static string FromCript(this string crypt)
+        {
+            try
+            {
+                return String.Join(" ", crypt.Split(' ').Select(s => s.Decrypt(key, iv)));
+            }
+            catch
+            {
+                return crypt;
+            }
+        }
+
+        private static string Encrypt(this string txt, byte[] key = null, byte[] iv = null)
+        {
+            string encrypted;
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(txt);
+                        }
+                        encrypted = Convert.ToBase64String(msEncrypt.ToArray());
+                    }
+                }
+            }
+            return encrypted;
+        }
+
+        public static string Decrypt(this string base64, byte[] key, byte[] iv)
+        {
+            // Declare the string used to hold
+            // the decrypted text.
+            string result = null;
+
+            byte[] cipherText = Convert.FromBase64String(base64);
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            result = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+    }
+
+}
